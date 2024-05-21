9670d356e19c418e71a5d4c37b1a5eeb8819020b
Gardiny <igor20091983@gmail.com>
2024-05-21 16:31:25 -0300

    java21

---
+++ src/Questao2.java
@@ -0,0 +1,24 @@
+package src;
+import java.util.Scanner;
+
+public class Questao2 {
+    public static void main(String[] args) {
+        System.out.println("Bem Vindo a loja de penhores");
+
+        float actionfigure = (float) 25.60;
+        double aviao = 15.60;
+        float Mario = (float) 35.60;
+
+
+        System.out.printf("1 - Action figure: RS%.2f\n", actionfigure);
+        System.out.printf("2 - Aviao: RS%.2f\n", aviao);
+        System.out.printf("3 - Mario: RS%.2f\n", Mario);
+
+        Scanner entradadedaos = new Scanner(System.in);
+
+        
+
+
+    }
+    
+}
---
+++ src/exemplo.java
@@ -0,0 +1,18 @@
+package src;
+public class exemplo {
+    public static void main(String [] args){
+        if (soma > 10) {
+            System.out.println("maior que 10");
+        }else{
+            System.out.println("Menor ou irgual a 10")
+        }
+        mensagem = (soma > 10) ? "maior q 10" : "menor ou igual a 10";
+        System.out.println(mensagem)
+
+        // int[] numeros = new int[5];
+        int[] numeros = {10,20,30,40,50};
+        for (int i = 0; i < 5; i++){
+            System.out.println("Posiçao [" + (1+1)+ "]:"+numeros[i])
+        }
+    } 
+}
